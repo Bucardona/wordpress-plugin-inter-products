83f6bc8d0289390f724761fcd5913d29cce08286
Mona Lisa <bucardona@gmail.com>
2024-04-18 11:56:14 -0500

    actualización

--- includes/template-handler.php
+++ includes/template-handler.php
@@ -3,6 +3,8 @@ function inter_products_template_redirect($template)
 {
   $page_slug = get_query_var('inter_custom_page', false);
 
+  var_dump($page_slug);
+
   if ($page_slug) {
     $template = locate_template("elementor-templates/{$page_slug}.php");
   }
