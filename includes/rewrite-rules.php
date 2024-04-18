4c0e09a56ad44cd93b6f00f2ba9db81e8fe08a51
Mona Lisa <bucardona@gmail.com>
2024-04-18 11:53:04 -0500

    actualización de rewrite

--- includes/rewrite-rules.php
+++ includes/rewrite-rules.php
@@ -1,9 +1,9 @@
 <?php
 function inter_products_add_rewrite_rules()
 {
-  add_rewrite_rule('^productos/?$', 'index.php?inter_custom_page=productos', 'top');
-  add_rewrite_rule('^productos/([^/]*)/?$', 'index.php?inter_custom_page=productos_single&inter_product_name=$matches[1]', 'top');
-  add_rewrite_rule('^productos/([^/]*)/([^/]*)/?$', 'index.php?inter_custom_page=productos-$matches[1]&inter_product_loop=$matches[1]&inter_product_loop_name=$matches[2]', 'top');
+  add_rewrite_rule('productos/?$', 'index.php?inter_custom_page=productos', 'top');
+  add_rewrite_rule('productos/([^/]*)/?$', 'index.php?inter_custom_page=productos_single&inter_product_name=$matches[1]', 'top');
+  add_rewrite_rule('productos/([^/]*)/([^/]*)/?$', 'index.php?inter_custom_page=productos-$matches[1]&inter_product_loop=$matches[1]&inter_product_loop_name=$matches[2]', 'top');
 }
 
 function inter_products_query_vars($vars)
