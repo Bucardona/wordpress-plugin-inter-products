12d71a7ed1cdf03eb68956fa01eb8fa0169d808c
Mona Lisa <bucardona@gmail.com>
2024-04-19 08:43:32 -0500

    actualización templates

--- includes/productos/elementor-templates/productos-categorias.php
+++ includes/productos/elementor-templates/productos-categorias.php
@@ -4,8 +4,14 @@ get_header();
 
 // Prueba temporal
 $page_slug = get_query_var('inter_custom_page', false);
-if ($page_slug) {
-  echo $page_slug;
+if ($page_slug && $page_slug === 'productos-categorias') {
+  $product_loop = get_query_var('inter_product_loop', false);
+  $product_category = get_query_vars('inter_product_loop_name', false);
+
+  if ($product_loop && $product_category) {
+
+    echo do_shortcode('[elementor-template id="940"]');
+  }
 }
 
 // Get the footer of the WordPress theme.
