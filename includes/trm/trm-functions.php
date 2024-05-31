37902be919436349d99e71c6d74fb306e3154886
Mona Lisa <bucardona@gmail.com>
2024-05-31 16:27:50 -0500

    update get trm function

--- includes/trm/trm-functions.php
+++ includes/trm/trm-functions.php
@@ -11,15 +11,42 @@ class InterTRM
   public function getTrmApi()
   {
     $apiUrl = $this->getTrmApiUrl();
-    $response = wp_remote_get($apiUrl/*, array('headers' => ['Cache-Control' => 'no-cache'])*/);
+
+    // Inicializa una sesión cURL
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language: es-419,es;q=0.9,en;q=0.8',
+      'cache-control: max-age=0',
+      'sec-ch-ua: \"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"',
+      'sec-ch-ua-mobile: ?0',
+      'sec-ch-ua-platform: \"Windows\"',
+      'sec-fetch-dest: document',
+      'sec-fetch-mode: navigate',
+      'sec-fetch-site: none',
+      'sec-fetch-user: ?1',
+      'upgrade-insecure-requests: 1',
+      'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    ]);
+
+    // Ejecuta la solicitud cURL y obtiene la respuesta
+    $response = curl_exec($ch);
+
+    // Cierra la sesión cURL
+    curl_close($ch);
+
+    /*$response = wp_remote_get($apiUrl, array('headers' => ['Cache-Control' => 'no-cache',]));
 
     if (is_wp_error($response)) {
       error_log('Error fetching TRM: ' . $response->get_error_message());
       return false;
     }
 
-    $body = wp_remote_retrieve_body($response);
-    $data = json_decode($body, true);
+    $body = wp_remote_retrieve_body($response);*/
+    $data = json_decode($response, true);
 
     if (isset($data['data']) && $data['data']['value']) {
       $trm = (float) $data['data']['value'];
